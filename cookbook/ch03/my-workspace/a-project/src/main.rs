06006fda20d1e33dda1a6033215dfcd30093e1cb
Seongjung Kim <vder8282@gmail.com>
2022-03-09 09:16:02 +0900

    Revert "Merge pull request #1 from SeongjungKim/dev"
    This reverts commit d36d40066e4ce911f310ae3f6505cddaae4c8f9b, reversing
    changes made to c9238dea8737acaf95c7607210f42cf5ab893f41.

--- cookbook/ch03/my-workspace/a-project/src/main.rs
+++
@@ -1,6 +0,0 @@
-use a_lib::stringify;
-use rand::prelude::*;
-
-fn main() {
-    println!("{{ \"values\": {}, \"sensor\": {} }}", stringify(&vec![random::<f64>(); 6]), stringify(&"temperature"));
-}
